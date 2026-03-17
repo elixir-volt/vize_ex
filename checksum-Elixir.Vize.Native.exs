eac9fc486c14b45b8a8f012f92d8c24262c63391
Danila Poyarkov <dev@dannote.net>
2026-03-17 15:21:32 +0300

    Add v0.7.0 precompiled NIF checksums

--- checksum-Elixir.Vize.Native.exs
+++ checksum-Elixir.Vize.Native.exs
@@ -1,1 +1,7 @@
-%{}
+%{
+  "libvize_ex_nif-v0.7.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:86aff8c708778a1127df7f437a4f6b11de0afe45505e2ac6b35a03f1c81485f2",
+  "libvize_ex_nif-v0.7.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:931cfc4cb98a8f05729ac2f6f5bbca3144c593090b804832fbfcf4541ba09957",
+  "libvize_ex_nif-v0.7.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:5f34e5c318df6dfdc00fd7f54ce7e72ca12c0eb8260a0942b624b6e4cd458047",
+  "libvize_ex_nif-v0.7.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d37a5ad5b381396bb4d5d10797d871695e2a464d9084232b22c4f4aba5037088",
+  "libvize_ex_nif-v0.7.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:18ee3583c99c7a58892562c68753869af12d322c18cbf4b8d28cd08575a285a1",
+}
