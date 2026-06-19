d8c3fe5bc71fd2c35dc33b3041d8f7a0d7edf902
Danila Poyarkov <dev@dannote.net>
2026-06-19 11:24:26 +0200

    Update precompiled NIF checksums for v0.12.0

--- checksum-Elixir.Vize.Native.exs
+++ checksum-Elixir.Vize.Native.exs
@@ -1,7 +1,7 @@
 %{
-  "libvize_ex_nif-v0.11.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:5ce0487b0e50a6b1572bc200f48bcc6fdc4f5f81003323eb638c36bd1372ac34",
-  "libvize_ex_nif-v0.11.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:8bfc4260ebde81cb5e7b8df23ab4f49cae9fa547e482bd806c194fc46dece079",
-  "libvize_ex_nif-v0.11.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:d049e93eb0079a6c775d53770c75bd09e119f6cbae3c41ee3d998567cf9a4c8c",
-  "libvize_ex_nif-v0.11.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:99c857ce192af4b95f923a85af39469b8d5495239c35d5b24729824c31576b68",
-  "libvize_ex_nif-v0.11.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:a6ace4369b47cb2a3b497625442e3489b3d37f23b6ece701e7d4e78d8764f3b8",
+  "libvize_ex_nif-v0.12.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:404afc28c8c40f9dadf79100a2c495c41bcfb7fe143d4564bafb6c77d89aae7f",
+  "libvize_ex_nif-v0.12.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:b0f0a3bd4518fea0bc7607493618a6d5b30295cc57eebbbc49f04ee6e7da8d54",
+  "libvize_ex_nif-v0.12.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:07fdf858b2ea860ffe141ccc4106d60387f87f37193bea3d193429cd6f3f2d97",
+  "libvize_ex_nif-v0.12.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:7d55756d6f96a34ebe01b726ac7c92a5428bee370a11702a22174c120378ec02",
+  "libvize_ex_nif-v0.12.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:5b29175ba989e8d36eae6e05abf78468d844f391deb7bd4a92dfb1fdfe312791",
 }
